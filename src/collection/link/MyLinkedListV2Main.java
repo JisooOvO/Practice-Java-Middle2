8ae3f3b4c59ad1f65827208455eb1d4c9a7beac3
Amuz-Jisoo <jisoo@amuz.co.kr>
2025-03-29 19:20:03 +0900

    [Study] linked list

---
+++ src/collection/link/MyLinkedListV2Main.java
@@ -0,0 +1,26 @@
+package collection.link;
+
+public class MyLinkedListV2Main {
+
+    public static void main(String[] args) {
+        MyLinkedListV2 list = new MyLinkedListV2();
+
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list);
+
+        // 첫 번째 항목에 추가, 삭제
+        list.add(0, "d");
+        System.out.println(list);
+        list.remove(0);
+        System.out.println(list);
+
+        // 중간 항목에 추가, 삭제
+        list.add(1, "e");
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+    }
+
+}
